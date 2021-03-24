2fe2b4458c2972c35fef5572c8e8450f4166ecb8
Nate Sales <nate@natesales.net>
2021-03-23 18:17:00 -0700

    make PoPs route generic for users

--- api.py
+++ api.py
@@ -230,15 +230,20 @@ async def delete_vm(vm_id: str, x_token: Optional[str] = Header(None), api_key: 
     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Unable to delete VM")
 
 
-@app.get("/admin/pops")
+@app.get("/pops")
 async def get_pops(x_token: Optional[str] = Header(None), api_key: Optional[str] = Cookie(None)):
-    user_doc = await db["users"].find_one({"api_key": x_token if x_token else api_key, "admin": True})
+    user_doc = await db["users"].find_one({"api_key": x_token if x_token else api_key})
     if not user_doc:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized")
 
     pops = []
     async for pop in db["pops"].find():
-        pops.append(pop)
+        if user_doc.get("admin"):
+            pops.append(pop)
+        else:
+            del pop["hosts"]
+            del pop["_id"]
+            pops.append(pop)
 
     return Response(status_code=status.HTTP_200_OK, content=pops)
 
