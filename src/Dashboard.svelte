393a742f58653554f614d244695a01057a83d554
Nate Sales <nate@natesales.net>
2021-03-27 00:36:48 -0700

    redirect to projects create page or their first project

--- src/Dashboard.svelte
+++ src/Dashboard.svelte
@@ -1,10 +1,10 @@
 <script>
-	import Router from "svelte-spa-router";
+	import Router, { push } from "svelte-spa-router";
 	import Create from "./pages/Create.svelte";
 	import Project from "./pages/Project.svelte";
 	import NewProject from "./pages/NewProject.svelte";
 	import Sidebar from "./components/Sidebar.svelte";
-	import { User } from "./stores";
+	import { Projects, User } from "./stores";
 	import { onMount } from "svelte";
 	import Resource from "./pages/Resource.svelte";
 
@@ -19,6 +19,13 @@
 	onMount(() => {
 		if ($User == {} || $User == null || $User == "undefined") {
 			console.log("redirected");
+		}
+
+		// Redirect to projects create page if user doesn't have any projects, otherwise redirect to their first project
+		if ($Projects.length < 1) {
+			push("/dashboard/projects/create")
+		} else {
+			push("/dashboard/projects/" + $Projects[0]._id)
 		}
 	});
 </script>
