c162f4b5a848cd42315eb236ecf04fb0b2ceacbf
Nate Sales <nate@natesales.net>
2021-03-27 01:15:28 -0700

    delete aarch64 suffix

--- load.py
+++ load.py
@@ -5,27 +5,27 @@ db = MongoClient("mongodb://localhost:27017")["aarch64"]
 db["config"].update_one({}, {"$set": {
     "prefix": "2001:db8::/42",
     "plans": {
-        "v1.xsmall.aarch64": {
+        "v1.xsmall": {
             "vcpus": 1,
             "memory": 1,
             "disk": 4
         },
-        "v1.small.aarch64": {
+        "v1.small": {
             "vcpus": 2,
             "memory": 4,
             "disk": 8
         },
-        "v1.medium.aarch64": {
+        "v1.medium": {
             "vcpus": 4,
             "memory": 8,
             "disk": 16
         },
-        "v1.large.aarch64": {
+        "v1.large": {
             "vcpus": 8,
             "memory": 16,
             "disk": 32
         },
-        "v1.xlarge.aarch64": {
+        "v1.xlarge": {
             "vcpus": 16,
             "memory": 32,
             "disk": 64
