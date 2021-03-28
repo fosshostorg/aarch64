c7023d4c3affbec727f751668b316c3092ed8da7
Nate Sales <nate@natesales.net>
2021-03-28 16:14:35 -0700

    provisioning: fix VM auditing

--- provisioning/files/audit.py
+++ provisioning/files/audit.py
@@ -1,24 +1,28 @@
 import os
 
+
+def destroy(vm):
+    print("Destroying " + vm)
+    os.system(f"virsh destroy {vm}")
+    os.system(f"virsh undefine --nvram {vm}")
+    os.system(f"rm -rf /opt/aarch64/vms/{vm}*")
+
+
 auth_vms = []
 with open("/tmp/authorized-vms.txt", "r") as auth_vms_file:
     for vm in auth_vms_file:
-        auth_vms.append(vm)
+        auth_vms.append(vm.strip())
 
 auth_ok = True
 for vm in os.popen("virsh list --all | tail -n +3").read().strip().split("\n"):
-    parts = vm.replace("  ", "").split(" ")
+    vm = vm.split(" ")
+    while "" in vm:
+        vm.remove("")
 
-    while "" in parts:
-        parts.remove("")
-
-    if len(parts) > 2:
-        if parts[1] not in auth_vms:
+    if len(vm) > 0:
+        if vm[1] not in auth_vms:
             auth_ok = False
-            print(f"VM {parts[1]} is not defined, destroying")
-            os.system(f"virsh destroy {parts[1]}")
-            os.system(f"virsh undefine --nvram {parts[1]}")
-            os.system(f"rm -rf /opt/aarch64/vms/{parts[1]}*")
+            destroy(vm[1])
 
 if not auth_ok:
-    exit(1)
+    exit(2)
