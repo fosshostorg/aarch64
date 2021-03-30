9997907ab3a20e771be170a92c535998caf079c8
Nate Sales <nate@natesales.net>
2021-03-30 13:42:21 -0700

    provisioning: add VM bridge interface auditing

--- provisioning/files/audit.py
+++ provisioning/files/audit.py
@@ -13,6 +13,11 @@ with open("/tmp/authorized-vms.txt", "r") as auth_vms_file:
     for vm in auth_vms_file:
         auth_vms.append(vm.strip())
 
+auth_ifaces = []
+with open("/tmp/authorized-ifaces.txt", "r") as auth_ifaces_file:
+    for iface in auth_ifaces_file:
+        auth_ifaces.append(iface.strip())
+
 auth_ok = True
 for vm in os.popen("virsh list --all | tail -n +3").read().strip().split("\n"):
     vm = vm.split(" ")
@@ -24,5 +29,13 @@ for vm in os.popen("virsh list --all | tail -n +3").read().strip().split("\n"):
             auth_ok = False
             destroy(vm[1])
 
-if not auth_ok:
+# Audit network interfaces
+ifaces_ok = True
+for iface in os.listdir("/sys/class/net/"):
+    if iface.startswith("vbr"):  # If VM bridge
+        if iface not in auth_ifaces:
+            os.system(f"ip link del {iface}")  # TODO: Netlink instead of calling ip
+            ifaces_ok = False
+
+if not (auth_ok and ifaces_ok):
     exit(2)
