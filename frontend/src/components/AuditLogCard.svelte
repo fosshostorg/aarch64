b1d80a610df391deba645fe84d6b8aaca972ecad
Seth Knights <knightss@catlin.edu>
2021-05-22 21:10:57 -0700

    Project-based audit logs.

---
+++ frontend/src/components/AuditLogCard.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let log: Log;
+  export let isProjectLevel: boolean;
+
+  let time = new Date(log.time);
+  let parts = time.toLocaleString("en-US");
+  let timeString = parts.split(',').join(' at ');
+</script>
+
+<div>
+  <span>
+    <p class="action"><b>ACTION</b>: {log.title}</p>
+    <p class="time">{timeString}</p>
+  </span>
+  <p><b>USER</b>: {log.user_email}</p>
+  {#if !isProjectLevel}
+    <p><b>PROJECT</b>: {log.project_name}</p>
+  {/if}
+  <p><b>DETAILS</b>: {JSON.stringify(log.detail)}</p>
+</div>
+
+<style>
+  div {
+    width: 100%;
+    max-width: 800px;
+    border: 1px solid black;
+    padding: 0.5rem 1rem;
+    margin: 1rem auto 0rem;
+  }
+
+  p {
+    margin: 0px;
+  }
+
+  span {
+    display: flex;
+    padding-bottom: 0.5rem;
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 0.5rem;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  p.time {
+    
+  }
+</style>
---
+++ frontend/src/pages/AuditLog.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import AuditLogCard from '../components/AuditLogCard.svelte';
+  import {location} from "svelte-spa-router";
+import { onMount } from 'svelte';
+import Spinner from '../components/Spinner.svelte';
+
+  export let admin: boolean;
+  export let params: any;
+
+  let logs: Log[] = null;
+
+  let isProjectLevel = $location.includes("project");
+
+  const getLogs = async () => {
+    fetch(`__apiRoute__/project/${params.project_id}/audit`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.meta.success) {
+        logs = data.data;
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  onMount(() => {
+    getLogs();
+  })
+</script>
+
+<main>
+  {#if logs == null}
+    <div>
+      <Spinner />
+    </div>
+  {:else}
+  {#each logs as log}
+    <AuditLogCard {log} {isProjectLevel} />
+  {/each}
+  {/if}
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    padding-left: 1rem;
+  }
+
+  div {
+    margin: 2rem auto;
+  }
+</style>
