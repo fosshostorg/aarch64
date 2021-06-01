b83c7845d2100bafb7a61fdf052d1b75011bb7cd
Hampton Moore <me@hampton.pw>
2021-06-01 17:18:23 -0400

    Loop through all hypervisors

--- api.py
+++ api.py
@@ -940,18 +940,25 @@ def domain_to_dict(vm):
 def libvirt_state_manager():
     while True:
         time.sleep(10)
-        try:
-            conn = libvirt.open('qemu+tcp://root@[fd0d:944c:1337:aa64:33c::]:16509/system')
-        except libvirt.libvirtError as e:
-            print(e, file=sys.stderr)
-        vms = conn.listAllDomains(0)
-        for vm in vms:
-            result = db["vms"].update_one(
-                {"_id": to_object_id(vm.name())},
-                {
-                    "$set": {"state": vm.info()[0]}
-                }
-            )
+        hypervisors = []
+        for pop in db["pops"].find():
+            if pop.get("hosts"):
+                for idx, host in enumerate(pop.get("hosts")):
+                    hypervisors.append(prefix_to_wireguard(host["prefix"]))
+
+        for hypervisor in hypervisors:
+            try:
+                conn = libvirt.open(f'qemu+tcp://root@[{hypervisor}]:16509/system')
+            except libvirt.libvirtError as e:
+                print(e, file=sys.stderr)
+            vms = conn.listAllDomains(0)
+            for vm in vms:
+                result = db["vms"].update_one(
+                    {"_id": to_object_id(vm.name())},
+                    {
+                        "$set": {"state": vm.info()[0]}
+                    }
+                )
     
 
 
