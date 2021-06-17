19389e9f5e079894aa664f3dd9783c51a17fb67a
Nate Sales <nate@natesales.net>
2021-06-17 16:18:28 -0700

    add feedback link

--- frontend/src/components/Sidebar.svelte
+++ frontend/src/components/Sidebar.svelte
@@ -21,6 +21,7 @@
 				// { name: "Settings", route: "/account/settings" },
 				// { name: "API", route: "https://github.com/", isGlobal: true },
 				{ name: "Docs", route: "/docs" },
+				{ name: "Feedback", route: "https://roadmap.aarch64.com/aarch64" },
 			],
 			open: true,
 		},
