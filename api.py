c3c733dc1f2fe46897828cfd07ab709f76f1dde5
GrahamSH <grahamshllk@gmail.com>
2022-01-31 15:26:15 +0000

    Add rename hostname feature

--- api.py
+++ api.py
@@ -714,6 +714,25 @@ def nat_vm(json_body: dict, user_doc: dict) -> Response:
         return _resp(True, "NAT has been added")
     return _resp(False, "Unable to add NAT")
 
+@app.route("/vms/rename", methods=["POST"])
+@with_authentication(admin=False, pass_user=True)
+@with_json("vm", "newHostname")
+def rename_vm(json_body: dict, user_doc: dict):
+    newHostname = json_body["newHostname"]
+    if type(newHostname) is not str:
+                return _resp(False, "Invalid type for newHostname parameter")
+    vm_doc = db["vms"].find_one({"_id": to_object_id(json_body["vm"])})
+    if not vm_doc:
+        return _resp(False, "VM doesn't exist")
+
+    project_doc = get_project(user_doc, vm_doc["project"])
+    if not project_doc:
+        return _resp(False, "Project doesn't exist or unauthorized")
+    db["vms"].update_one({"_id": to_object_id(json_body["vm"])}, { "$set": { 'hostname': newHostname } })
+
+    add_audit_entry("vm.rename", project_doc["_id"], user_doc["_id"], "", "")
+    return _resp(True, "VM has been renamed")
+
 @app.route("/vms/create", methods=["POST"])
 @with_authentication(admin=False, pass_user=True)
 @with_json("hostname", "plan", "pop", "project", "os")
